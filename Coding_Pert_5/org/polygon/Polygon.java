80a3177b519f18db3e2dcc28b9ca644567df74ed
AbyanDzh <100134850+AbyanDzh@users.noreply.github.com>
2023-03-15 19:27:02 +0700

    Add files via upload

---
+++ Coding_Pert_5/org/polygon/Polygon.java
@@ -0,0 +1,23 @@
+/**
+* File : polygon.java 02/04/2014
+* Penulis 
+
+*/
+package org.polygon;
+
+public class Polygon{
+	protected int jumlahSisi;
+
+	public Polygon(){
+		this.jumlahSisi = 0;
+	}
+
+	public void setJumlahSisi(int jumlahSisi){
+		this.jumlahSisi = jumlahSisi;
+	}
+
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+
+}
